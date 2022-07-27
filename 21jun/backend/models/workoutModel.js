8f3364def3445f322fc8b3bb71b2dcdbfa77113d
zahra-moeini <z.sh.moeini1379@gmail.com>
2022-07-27 15:29:37 +0430

    comitt1

--- 21jun/backend/models/workoutModel.js
+++ 21jun/backend/models/workoutModel.js
@@ -1,20 +1,20 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-const Schema=mongoose.Schema
+const Schema = mongoose.Schema
 
-const workoutSchema=new Schema({
-    title:{
-        type:String,
-        required:true
-     },
-     reps:{
-        type:Number,
-        required:true
-     },
-     load:{
-        type:Number,
-        required:true
-     },
-},{timestamps:true})
+const workoutSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  reps: {
+    type: Number,
+    required: true
+  },
+  load: {
+    type: Number,
+    required: true
+  }
+}, { timestamps: true })
 
-module.exports=mongoose.model('workout',workoutSchema)
+module.exports = mongoose.model('Workout', workoutSchema)
